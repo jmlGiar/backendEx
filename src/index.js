3ec30b3de06ae2aa2027734903bd5233151b51e3
jmlGiar <dexvionicle@gmail.com>
2023-02-09 20:53:38 -0300

    final v.1.1

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(PORT, () => {
     console.log('Server is running on port ' +PORT)
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'))
-})
+});
 
 app.get('/productos', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productos.html'))
-})
+});
 
 app.get('/productos/1', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/productos1.html'))
-})
+});
